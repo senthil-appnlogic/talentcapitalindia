e87848bf3dabd8ab19e7ad8f4bde96ecf287ed7e
senthil-appnlogic <sentilkumar@appnlogic.com>
2016-08-25 12:05:09 +0530

    Talent Capital India Updated

--- views/view/index.php
+++ views/view/index.php
@@ -48,7 +48,7 @@
     
 <html lang="en">
     <head>
-        <title>Talent India</title>
+        <title>Talent Capital India</title>
         <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="icon" href="images/favicon.ico" type="image/gif"> 
